159b98b5b2d3d2cef06a417a5490d1d9e252b4ef
hamzamaimi <hamzamaimi0901@gmail.com>
2023-01-21 13:46:57 +0100

    feat: added reactRouters to project and to App.tsx, added registration screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import './App.css';
 import {Login} from "./screens/Login";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {Registration} from "./screens/Registration";
 
 function App() {
+    // return (
+    //     <>
+    //         <Login/>
+    //     </>
+    // );
     return (
         <>
-            <Login/>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/registration" element={<Registration />} />
+                    <Route path="" element={<Login />} />
+                </Routes>
+            </BrowserRouter>
         </>
-    );
+    )
 }
 
 export default App;
--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,8 +1,5 @@
 import React from "react";
 import axios from 'axios';
-import {Simulate} from "react-dom/test-utils";
-import compositionStart = Simulate.compositionStart;
-import {data} from "jquery";
 
 export const LoginForm = (props : {width : string}) => {
 
