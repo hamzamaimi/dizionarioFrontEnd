8b0ff5b57a9bce1099500321a5962c61781e8080
hamzamaimi <hamzamaimi0901@gmail.com>
2023-01-24 19:37:50 +0100

    feat: added state in loginForm component that contain error

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,14 +1,16 @@
-import React from "react";
+import React, {useState} from "react";
 import axios from 'axios';
 import {AlternativeAuthenticationMethods} from "./AlternativeAuthenticationMethods";
 import {AuthenticationButton} from "../microComponents/AuthenticationButton";
 
 export const LoginForm = () => {
+    const [error, setError] = useState("");
 
     return(
         <div className={"mx-auto authentication-form-container"}>
             <form onSubmit={(e) => checkForm(e)}>
-                <div id="credenzialiErrate" className='alert alert-danger d-none' role='alert'>Credenziali errate!</div>
+                {error !== "" ? (<div id="credenzialiErrate" className='alert alert-danger' role='alert'>{error}</div>) : ''}
+                
                 <div className="mb-3 text-start">
                     <label className="login-form-label mb-3">Email</label>
                     <input type="email" className="form-control" id="inputEmail" required/>
@@ -28,25 +30,35 @@ export const LoginForm = () => {
             </form>
         </div>
     )
-}
 
-let checkForm = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    login();
-}
 
-function login() {
-    let email:string = (document.getElementById("inputEmail") as HTMLInputElement).value;
-    let password:string = (document.getElementById("InputPassword") as HTMLInputElement).value;
+    function login() {
+        let email:string = (document.getElementById("inputEmail") as HTMLInputElement).value;
+        let password:string = (document.getElementById("InputPassword") as HTMLInputElement).value;
 
-    axios.post("http://localhost:8080/login", { "email": email, "password": password}).then(res => {
-        if (res.data.hasOwnProperty("error")) {
-            let wrongCredentials = document.getElementById("credenzialiErrate");
-
-            if(wrongCredentials !== null){
-                wrongCredentials.classList.remove("d-none")
+        axios.post("http://localhost:8080/login", { "email": email, "password": password}).then(res => {
+            if (res.data.hasOwnProperty("error")) {
+                handleError(res.data.error);
             }
-        }
 
-    })
+        })
+    }
+
+    function checkForm(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+        login();
+    }
+
+    function handleError(error: string) {
+        switch(error){
+            case "email or password is missing.":
+                setError('Inserisci Email e Password!');
+                break;
+            case "Email or password is wrong":
+                setError('Credenziali errate!')
+                break;
+        }
+               
+    }
 }
+
