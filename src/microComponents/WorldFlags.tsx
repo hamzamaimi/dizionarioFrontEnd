b0d1d32fdaceb586b1b098efcb72ae7a1acbacc3
hamzamaimi <hamzamaimi0901@gmail.com>
2023-02-12 22:25:39 +0100

    feat: added homepage component

---
+++ src/microComponents/WorldFlags.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+export const WorldFlags = () => {
+    return(<>
+        <div className="row text-center">
+            <div className="col-4 col-lg-2">
+                <img className="m-width-70" src="images/bandieraCina.png" alt="bandiera cina"/>
+            </div>
+            <div className="col-4 col-lg-2">
+                <img className="m-width-70" src="images/bandieraGermania.png" alt="bandiera germania"/>
+            </div>
+            <div className="col-4 col-lg-2">
+                <img className="m-width-70" src="images/bandieraMessico.png" alt="bandiera messico"/>
+            </div>
+            <div className="col-4 col-lg-2">
+                <img className="m-width-70" src="images/bandieraPaesiBassi.png" alt="bandiera paesi bassi"/>
+            </div>
+            <div className="col-4 col-lg-2">
+                <img className="m-width-70" src="images/bandieraPortogallo.png" alt="bandiera portogallo"/>
+            </div>
+            <div className="col-4 col-lg-2">
+                <img className="m-width-70" src="images/bandieraTurchia.png" alt="bandieraturchia"/>
+            </div>
+        </div>
+    </>)
+}
--- src/screens/HomePage.tsx
+++ src/screens/HomePage.tsx
@@ -1,10 +1,51 @@
 import React from "react";
-import { LogOut } from "../components/LogOut";
+// import { LogOut } from "../components/LogOut";
+import { ProjectLogo } from "../microComponents/ProjectLogo";
+import { WorldFlags } from "../microComponents/WorldFlags";
+import "../styles/Application.css"
 
 export const HomePage = () => {
 
     return(<>
-         HomePage
-         <LogOut /> 
+        <div className={"customContainer container-fluid"}>
+            <div className="row">
+                <div className="col-12 col-md-2"></div>
+                <div className="col-12 col-md-8">
+                    <div className="row">
+                        <div className="col-md-2 col-xxl-4 col-12"></div>
+                        <div className="col-md-8 col-xxl-4 col-12">
+                            <ProjectLogo width={"100%"}/>
+                        </div>
+                        <div className="col-md-2 col-xxl-4 col-12"></div>
+                    </div>
+                </div>
+                <div className="col-12 col-md-2"></div>
+            </div>
+
+            <div className="row">
+                <div className="col-12 col-md-2"></div>
+                <div className="col-12 col-md-8 text-center">
+                    <div className="row">
+                        <div className="card col-12 mt-5">
+                            <div className="card-body">
+                                <h5 className="card-title">Gestisci tabelle</h5>
+                                <p className="card-text">Inserisci, modifica o elimina parole e tabelle.</p>
+                                <button className="btn btn-primary">Gestisci</button>
+                            </div>
+                        </div>
+
+                        <div className="card col-12 mt-5">
+                            <div className="card-body">
+                                <h5 className="card-title">Fai un quiz</h5>
+                                <p className="card-text">Esercitati utilizzando le parole che hai inserito nelle tue tabelle.</p>
+                                <button className="btn btn-primary">Esercitati</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-12 col-md-2"></div>
+            </div>
+            <WorldFlags />
+        </div>
     </>)
 }
