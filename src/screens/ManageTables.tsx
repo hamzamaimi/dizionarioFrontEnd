786788f5ea3c08752eb201cd1d0574711be6bd8b
hamzamaimi <hamzamaimi0901@gmail.com>
2023-02-13 14:40:20 +0100

    feat: added switch and table in ManageTables component

--- src/screens/ManageTables.tsx
+++ src/screens/ManageTables.tsx
@@ -1,5 +1,57 @@
 import React from "react";
+import { LogoWithLogOut } from "../components/LogoWithLogOut";
 
-export const ManageTables = () =>{
-    return(<>ManageTables</>)
-}   
+export const ManageTables = () => {
+  return (
+    <>
+      <div className={"customContainer container-fluid"}>
+        <LogoWithLogOut />
+
+        <div className="row">
+          <div className="col-4">
+            <div className="form-check form-switch">
+              <input className="form-check-input" type="checkbox" id="flexSwitchCheckDefault" />
+              <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Default switch checkbox input</label>
+            </div>
+          </div>
+          <div className="col-8">ciao</div>
+        </div>
+
+        <div className="row">
+          <div className="col-12">
+            <table className="table table-striped">
+              <thead>
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Parola</th>
+                  <th scope="col">Traduzione</th>
+                  <th scope="col">Tabella</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="row">1</th>
+                  <td>Mark</td>
+                  <td>Otto</td>
+                  <td>@mdo</td>
+                </tr>
+                <tr>
+                  <th scope="row">2</th>
+                  <td>Jacob</td>
+                  <td>Thornton</td>
+                  <td>@fat</td>
+                </tr>
+                <tr>
+                  <th scope="row">3</th>
+                  <td>Larry the Bird</td>
+                  <td>@twitter</td>
+                  <td>@twitter</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
