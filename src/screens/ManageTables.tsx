342647b5f9585fc9e6e7c5c777dc8dc9d0bf8707
hamzamaimi <hamzamaimi0901@gmail.com>
2023-02-19 22:36:11 +0100

    feat: added get request that return all translations and removed StrictMode render component which is the cause of double components rendering

--- src/screens/ManageTables.tsx
+++ src/screens/ManageTables.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "../api/axios";
 import { LogoWithLogOut } from "../components/LogoWithLogOut";
 
+
 export const ManageTables = () => {
+  const [translations, setTranslation] = useState(null);
+  const [groupName, setGroupName] = useState("");
+
+
+  useEffect (() => {
+      if(translations === null){
+      axios.get("/manageTranslation", {params: {"GroupName": groupName} , withCredentials: true}).then((resp)=>{
+        setTranslation(resp.data.translations)
+      })
+    }
+  }, [])
+
   return (
     <>
       <div className={"customContainer container-fluid"}>
@@ -11,7 +25,6 @@ export const ManageTables = () => {
           <div className="col-4">
             <div className="form-check form-switch">
               <input className="form-check-input" type="checkbox" id="flexSwitchCheckDefault" />
-              <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Default switch checkbox input</label>
             </div>
           </div>
           <div className="col-8">ciao</div>
