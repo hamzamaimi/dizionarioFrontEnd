f4dd35157c14f42c67cae1c2b88d35cdadd2b141
hamzamaimi <hamzamaimi0901@gmail.com>
2023-03-22 22:42:30 +0100

    feat: added call to manageTranslation servlet by using a get api

--- src/screens/DoQuiz.tsx
+++ src/screens/DoQuiz.tsx
@@ -1,25 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "../api/axios";
 import { LogoWithLogOut } from "../components/LogoWithLogOut";
 import { WorldFlags } from "../microComponents/WorldFlags";
 
 export const DoQuiz = () => {
+    const [translations, setTranslation] = useState({});
+    const [groupName, setGroupName] = useState("");
 
     $( document ).ready(function() {
         const availableHeight = (window.innerHeight - Number($('#logoWithLogOut').height()) - Number($('#worldFlags').height()));
         $('#quizRow').height(availableHeight);
-        console.log(Number($('#logoWithLogOut').height()))
-        console.log(Number($('#worldFlags').height()))
     });
+
+    useEffect(() => {
+        if(groupName === ""){
+            axios.get("/manageTranslation", {withCredentials: true}).then((resp)=>{
+                setTranslation(resp.data.translations);
+                setGroupName(resp.data.groupNames);
+            })
+        }   
+    })
 
     return(<>
         <div className={"customContainer container-fluid"}>
             <LogoWithLogOut />
             <div id="quizRow" className="row">
                 <div className="col-12">
-                    <div className="text-center">parola1</div>
-                    <div className="text-center">parola2</div>
-                    <div className="text-center">
-                        <button>click</button>
+                    <div className="text-center mb-5">parola1</div>
+                    <div className="text-center mb-5">parola2</div>
+                    <div className="text-center mb-5">
+                        <button type="button" className="btn btn-primary">Primary</button>
                     </div>
                 </div>
             </div>
