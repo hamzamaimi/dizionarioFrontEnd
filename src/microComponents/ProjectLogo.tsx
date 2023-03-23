dbd92c49ad5207db9f390ef11eae1da20d8d242b
hamzamaimi <hamzamaimi0901@gmail.com>
2023-03-23 23:22:48 +0100

    feat: added function triggered by click on logo that redirect to homepage

--- src/microComponents/ProjectLogo.tsx
+++ src/microComponents/ProjectLogo.tsx
@@ -1,7 +1,11 @@
 import React from "react"
 import "../styles/Base_styles.css"
+import { useNavigate } from 'react-router-dom';
+
 export const ProjectLogo = (props: { width: any }) => {
+    const navigate = useNavigate();
+
     return(
-        <img alt="Wordsmemo logo" id="project-logo" src="images/WordsMemo.png" />
+        <img style={{cursor:"pointer"}} onClick={() => navigate('/', {replace:true})} alt="Wordsmemo logo" id="project-logo" src="images/WordsMemo.png" />
     )
 }
